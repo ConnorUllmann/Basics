8f665636551d3cd52f0d5459cb4a1ec003d46020
ConnorUllmann <c.ullmann@hotmail.com>
2018-03-01 23:57:40 -0500

    Add documentation for Rectangle functions and move collision tests over from Engine

---
+++ Basics.Test/RectangleTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Basics.Test
+{
+    public class RectangleTests
+    {
+        [Theory]
+        [InlineData(-3, 0, -4, -4, 8, 8, true)]
+        [InlineData(0, -3, -4, -4, 8, 8, true)]
+        [InlineData(3, 0, -4, -4, 8, 8, true)]
+        [InlineData(0, 3, -4, -4, 8, 8, true)]
+        [InlineData(-5, 0, -4, -4, 8, 8, false)]
+        [InlineData(0, -5, -4, -4, 8, 8, false)]
+        [InlineData(5, 0, -4, -4, 8, 8, false)]
+        [InlineData(0, 5, -4, -4, 8, 8, false)]
+        [InlineData(-1, 5, -1, -3, 1, 10, true)]
+        [InlineData(5, -1, -3, -1, 10, 1, true)]
+        [InlineData(0, 0, 0, 0, 0, 0, false)]
+        [InlineData(0, 0, -1, -1, 2, 2, true)]
+        [InlineData(-5, -5, -5, -5, 5, 5, true)]
+        [InlineData(0, 0, -5, -5, 5, 5, false)]
+        [InlineData(3, 3, 0, 0, 5, 5, true)]
+        [InlineData(0, 0, 0, 0, 5, 5, true)]
+        [InlineData(5, 5, 0, 0, 5, 5, false)]
+        public void RectangleCollide_ReturnSucceed(float _x, float _y, float _rx, float _ry, float _rw, float _rh, bool _inside = true)
+            => Assert.Equal(_inside, Rectangle.Collide(_x, _y, _rx, _ry, _rw, _rh));
+    }
+}
--- Basics/Rectangle.cs
+++ Basics/Rectangle.cs
@@ -21,30 +21,118 @@ namespace Basics
             H = _h;
         }
 
+        /// <summary>
+        /// Area of the rectangle
+        /// </summary>
         public float Area => W * H;
 
-        public bool Collides(float px, float py) 
-            => px >= X &&
-               py >= Y &&
-               px < X + W &&
-               py < Y + H;
-        public bool Collides(Rectangle b) => Collide(X, Y, W, H, b.X, b.Y, b.W, b.H);
-        public bool Collides(float bx, float by, float bw, float bh) => Collide(X, Y, W, H, bx, by, bw, bh);
+        /// <summary>
+        /// Checks if the given point is inside this rectangle
+        /// </summary>
+        /// <param name="px">x-position of point</param>
+        /// <param name="py">y-position of point</param>
+        /// <returns>Whether the point is inside this rectangle</returns>
+        public bool Collides(float px, float py) => Collide(px, py, X, Y, W, H);
+
+        /// <summary>
+        /// Checks if the given point is inside the given rectangle
+        /// </summary>
+        /// <param name="px">x-position of point</param>
+        /// <param name="py">y-position of point</param>
+        /// <param name="rx">x-position of rectangle</param>
+        /// <param name="ry">y-position of rectangle</param>
+        /// <param name="rw">width of rectangle</param>
+        /// <param name="rh">height of rectangle</param>
+        /// <returns>Whether the point is inside the rectangle</returns>
+        public static bool Collide(float px, float py, float rx, float ry, float rw, float rh)
+            => px >= rx &&
+               py >= ry &&
+               px < rx + rw &&
+               py < ry + rh;
+
+        /// <summary>
+        /// Checks if this rectangle collides with another rectangle
+        /// </summary>
+        /// <param name="r">Rectangle to check collisions against</param>
+        /// <returns>Whether this rectangle collides with another</returns>
+        public bool Collides(Rectangle r) => Collide(X, Y, W, H, r.X, r.Y, r.W, r.H);
+
+        /// <summary>
+        /// Checks if this rectangle collides with another rectangle
+        /// </summary>
+        /// <param name="rx">x-position of rectangle</param>
+        /// <param name="ry">y-position of rectangle</param>
+        /// <param name="rw">width of rectangle</param>
+        /// <param name="rh">height of rectangle</param>
+        /// <returns>Whether this rectangle collides with another</returns>
+        public bool Collides(float rx, float ry, float rw, float rh) => Collide(X, Y, W, H, rx, ry, rw, rh);
+
+        /// <summary>
+        /// Checks if one rectangle collides with another rectangle
+        /// </summary>
+        /// <param name="ax">x-position of the first rectangle</param>
+        /// <param name="ay">y-position of the first rectangle</param>
+        /// <param name="aw">width of the first rectangle</param>
+        /// <param name="ah">height of the first rectangle</param>
+        /// <param name="bx">x-position of the second rectangle</param>
+        /// <param name="by">y-position of the second rectangle</param>
+        /// <param name="bw">width of the second rectangle</param>
+        /// <param name="bh">height of the second rectangle</param>
+        /// <returns>Whether one rectangle collides with another</returns>
         public static bool Collide(float ax, float ay, float aw, float ah, float bx, float by, float bw, float bh)
             => ax + aw >= bx &&
                ay + ah >= by &&
                ax <= bx + bw &&
                ay <= by + bh;
 
-        public bool Inside(Rectangle b) => Inside(X, Y, W, H, b.X, b.Y, b.W, b.H);
-        public bool Inside(float bx, float by, float bw, float bh) => Inside(X, Y, W, H, bx, by, bw, bh);
+        /// <summary>
+        /// Checks if one rectangle collides with another rectangle
+        /// </summary>
+        /// <param name="a">the first rectangle</param>
+        /// <param name="b">the second rectangle</param>
+        /// <returns>Whether one rectangle collides with another</returns>
+        public static bool Collide(Rectangle a, Rectangle b) => Collide(a.X, a.Y, a.W, a.H, b.X, b.Y, b.W, b.H);
+
+        /// <summary>
+        /// Whether this rectangle is completely inside the given rectangle
+        /// </summary>
+        /// <param name="r">rectangle to see if this rectangle is inside of</param>
+        /// <returns>Whether this rectangle is inside the given rectangle</returns>
+        public bool Inside(Rectangle r) => Inside(X, Y, W, H, r.X, r.Y, r.W, r.H);
+
+        /// <summary>
+        /// Whether this rectangle is completely inside the given rectangle
+        /// </summary>
+        /// <param name="rx">x-position of rectangle</param>
+        /// <param name="ry">y-position of rectangle</param>
+        /// <param name="rw">width of rectangle</param>
+        /// <param name="rh">height of rectangle</param>
+        /// <returns>Whether this rectangle is inside the given rectangle</returns>
+        public bool Inside(float rx, float ry, float rw, float rh) => Inside(X, Y, W, H, rx, ry, rw, rh);
+
+        /// <summary>
+        /// Whether one rectangle is completely inside another rectangle
+        /// </summary>
+        /// <param name="ax">x-position of the enveloped rectangle</param>
+        /// <param name="ay">y-position of the enveloped rectangle</param>
+        /// <param name="aw">width of the enveloped rectangle</param>
+        /// <param name="ah">height of the enveloped rectangle</param>
+        /// <param name="bx">x-position of the enveloping rectangle</param>
+        /// <param name="by">y-position of the enveloping rectangle</param>
+        /// <param name="bw">width of the enveloping rectangle</param>
+        /// <param name="bh">height of the enveloping rectangle</param>
+        /// <returns>Whether one rectangle is inside another</returns>
         public static bool Inside(float ax, float ay, float aw, float ah, float bx, float by, float bw, float bh)
             => ax >= bx &&
                ay >= by &&
                ax + aw < bx + bw &&
                ay + ah < by + bh;
         
-        public void SetDimensions(Rectangle _rectangle)
+        /// <summary>
+        /// Sets this rectangle's position/dimensions to match the given rectangle
+        /// </summary>
+        /// <param name="_rectangle">rectangle to match</param>
+        public void MatchPositionAndDimensions(Rectangle _rectangle)
         {
             X = _rectangle.X;
             Y = _rectangle.Y;
